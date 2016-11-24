94d10646822ba2092bbf1025fa5c691fab66fd90
Andrew van Rooyen <wraith.andrew@gmail.com>
2016-11-24 23:57:06 +0200

    Make the random bot slightly better

--- MyBot.go
+++ MyBot.go
@@ -6,18 +6,22 @@ import (
 )
 
 
-func move (gameMap hlt.GameMap, location hlt.Location) hlt.Move {
+func move (playerTag int, gameMap hlt.GameMap, location hlt.Location) hlt.Move {
     site := gameMap.GetSite(location, hlt.STILL)
+    // Always STILL if moving won't achieve anything
     if site.Strength == 0 {
-        return hlt.Move {
-            Location: location,
-            Direction: hlt.Direction(hlt.STILL),
+        return hlt.Move {location, hlt.STILL}
+    }
+
+    // Avoid moving into a site with higher strength
+    var options []hlt.Direction
+    for _, direction := range(hlt.CARDINALS) {
+        option := gameMap.GetSite(location, direction)
+        if option.Owner == playerTag || site.Strength > option.Strength {
+            options = append(options, direction)
         }
     }
-    return hlt.Move {
-        Location: location,
-        Direction: hlt.Direction(rand.Int() % 5),
-    }
+    return hlt.Move {location, options[rand.Intn(len(options))]}
 }
 
 
@@ -30,7 +34,7 @@ func main () {
 			for x := 0; x < gameMap.Width; x++ {
 				loc := hlt.NewLocation(x,y)
 				if gameMap.GetSite(loc, hlt.STILL).Owner == conn.PlayerTag {
-					moves = append(moves, move(gameMap, loc))
+					moves = append(moves, move(conn.PlayerTag, gameMap, loc))
 				}
 			}
 		}
